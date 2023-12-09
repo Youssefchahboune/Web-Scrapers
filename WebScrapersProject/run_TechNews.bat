fc93dd1b322e6830b17b33305b1b252a5701f9f3
Youssef Chaboune <Chahbouney2000@outlook.fr>
2023-12-09 01:20:49 -0500

    added a batch script to my task scheduler to send me tech news every day

---
+++ WebScrapersProject/run_TechNews.bat
@@ -0,0 +1,4 @@
+:: Batch file to add to "Task Scheduler" to run at a specific time
+@echo off
+"C:\Users\Admin\AppData\Local\Programs\Python\Python312\python.exe" "C:\Users\Admin\Desktop\Web-Scrapers\WebScrapersProject\WebScraper_TechNews.py"
+pause
